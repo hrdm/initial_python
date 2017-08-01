50369328b76a81bcbb8258b37933e65c176ba0ee
hrdm <hrdmilton@gmail.com>
2017-07-31 22:14:54 -0500

    updated mindstroms.py

--- drawing/mindstorms.py
+++ drawing/mindstorms.py
@@ -1,40 +1,28 @@
 import turtle
 
-def draw_square():
-    brad = turtle.Turtle()
-    brad.shape("circle")
-    brad.color("#FF007F", "red")
-    brad.speed(1)
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
 
-    for side in xrange(4):
-        print(side)
-        brad.forward(100)
-        brad.right(90)
 
-def draw_circle():
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("blue")
-    angie.circle(100)
-
-def draw_triangle():
-    tom = turtle.Turtle()
-    tom.shape("turtle")
-    tom.color("green")
-
-    for side in xrange(3):
-        tom.forward(200)
-        tom.right(120)
-
-def draw_figures():
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("#F6cc88")
-
-    draw_square()
-    draw_triangle()
-    draw_circle()
+    #Create the turtle Brad - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("#FF007F", "red")
+    brad.speed(5)
+    for i in xrange(1,37):
+        draw_square(brad)
+        brad.right(10)
+    #Create the turtle Angie - Draws a circle
+    # angie = turtle.Turtle()
+    # angie.shape("arrow")
+    # angie.color("blue")
+    # angie.circle(100)
 
     window.exitonclick()
 
-
-draw_figures()
+draw_art()
